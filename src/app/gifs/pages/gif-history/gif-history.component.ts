386d7246943d5dde4f8faf7c89ab6acb4f06f3d5
Cristian Leon <cristian.leon.avila@gmail.com>
2025-05-22 17:02:35 -0500

    Gifs

--- src/app/gifs/pages/gif-history/gif-history.component.ts
+++ src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -1,13 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { GiphyService } from '../../services/giphy.service';
+import { JsonPipe } from '@angular/common';
+import { GifListComponent } from '../../components/gif-list/gif-list.component';
 
 @Component({
   selector: 'app-gif-history',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './gif-history.component.html',
   styleUrl: './gif-history.component.css'
 })
 export default class GifHistoryComponent {
 
+  // Se puede obtener los parámetros URL con un observable
   //query = inject(ActivatedRoute).params.subscribe(console.log);
+
+  gifService = inject(GiphyService);
+
+  images = computed(() => {
+    const record = this.gifService.searchHistory();
+    return record[this.query()];
+  });
+
+  query = toSignal(
+    inject(ActivatedRoute).params.pipe(
+      map( params => params['query'] )
+    )
+  );
 }
